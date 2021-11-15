e84baf6cfb9311e55db582831f917599c94bc9f5
pranit <patilpranit3112@gmail.com>
2021-11-16 01:44:08 +0530

    Done with login and Sign up

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7
+.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="main-container">
+    <div class="container">
+        <p class="login-text">Sign up  with social media</p>
+        <div class="login-social">
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i> Facebook </a>
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i> Twitter</a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i> GooglePlus</a>
+            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i> Linkedin</a>
+        </div>
+        <form class="login-email">
+            <p class="login-text">Sign up with email</p>
+            <div class="input-group">
+                <input type="Text" placeholder="Your Name" required>
+            </div>
+            <div class="input-group">
+                <input type="email" placeholder="Email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="input-group">
+                <input type="text" placeholder="Contact Number" required>
+            </div>
+            <div class="input-group">
+                <button class="btn">Sign In</button>
+            </div>
+        </form>
+    </div>
+</div>    
+</body>
+</html>
